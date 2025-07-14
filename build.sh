ce3b89a2bb196cbab350e3f679365397cd61ad4a
jmacey <jmacey@bournemouth.ac.uk>
2025-07-14 12:06:38 +0100

    updated build.sh for linux

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 pyside6-rcc resources.rcc -o resources_rc.py
-uv run pyinstaller --onefile --windowed --add-data 'resources_rc.py:.' --add-data 'PyProject.json:.' main.py --name PyProject
+uv run pyinstaller --onefile --windowed --add-data 'resources_rc.py:.' --add-data 'templates/PyProject.json:.' main.py --name PyProject
 cp dist/PyProject /public/devel/25-26/PyProject
-cp PyProject.json /public/devel/25-26/PyProject
-cp MainDialog.ui /public/devel/25-26/PyProject
+cp -r templates/ /public/devel/25-26/PyProject
 
 
-# cp appsereicon.png  /public/devel/25-26/AppsEre
-# cp AppsEre.desktop /public/devel/25-26/AppsEre
+cp pyproject.png  /public/devel/25-26/PyProject
